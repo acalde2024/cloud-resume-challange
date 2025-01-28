b6cf21ccd055215ac80df15ef3d138f045755d10
acalde2024 <antoniocalderon96@hotmail>
2025-01-28 00:04:46 -0600

    fixed script.js and added my URL

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("type your HTTPS://your-lambda-url-here");
+    let response = await fetch("https://o2a3wizh775shoai4uqsntdhju0cflyc.lambda-url.us-east-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = ' Views: ' + data;
 }
